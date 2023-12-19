d2e241546b87e0264a0a1880f0d905e52c809fb9
clerickbarrion <clerickbarrion@gmail.com>
2023-12-19 12:10:26 -0500

    fixed duplicate text

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const socket = io()
-    const form = document.querySelector('button')
+    const form = document.querySelector('form')
     const input = document.querySelector('textarea')
     const messages = document.getElementById('con-box')
-    form.addEventListener('click', ()=>{
+    form.addEventListener('submit', e=>{
+        e.preventDefault()
         if (input.value){
             socket.emit('chat message',input.value);
             input.value= ''
         }
-        socket.on('chat message', msg=>{
-            const p = document.createElement('p')
-            p.textContent = msg
-            messages.appendChild(p)
-        })
+    })
+    socket.on('chat message', msg=>{
+        const p = document.createElement('p')
+        p.textContent = msg
+        messages.appendChild(p)
     })
 })
